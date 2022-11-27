9f3d1a78978fdad0d91ec6efc0d05c3f06f7bbe3
bakhtawar09 <97089884+bakhtawar09@users.noreply.github.com>
2022-11-27 10:19:51 +0500

    JWT authentication using passport.js

--- connected/server/utilities/authenticationJWT.js
+++ connected/server/utilities/authenticationJWT.js
@@ -1,9 +1,54 @@
-const express = require('express');
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
+const { authenticate, getUserById } = require("./database");
 
-const router = express.Router();
+passport.use(
+  new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+    authenticate(email, password)
+      .then((user) => {
+        if (user) {
+          return done(null, user);
+        }
 
-router.get('/', (req, res) => {
-  res.json(req.user);
+        return done(null, false);
+      })
+      .catch((err) => done(err));
+  })
+);
+
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    },
+    (jwtPayload, done) => {
+      const { userId } = jwtPayload;
+
+      getUserById(userId)
+        .then((user) => {
+          if (user) {
+            return done(null, user);
+          }
+
+          return done(null, false);
+        })
+        .catch((err) => {
+          done(err);
+        });
+    }
+  )
+);
+
+passport.serializeUser((user, cb) => {
+  process.nextTick(() => {
+    cb(null, user);
+  });
 });
 
-module.exports = router;
+passport.deserializeUser((user, cb) => {
+  process.nextTick(() => cb(null, user));
+});
+
+module.exports = passport;
