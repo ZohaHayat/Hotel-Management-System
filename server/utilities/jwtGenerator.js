9a3b3b00059cf4b9630d6e57ef09719fe4bdc580
bakhtawar09 <24100301@lums.edu.pk>
2022-11-28 01:08:57 +0500

    comments added

--- server/utilities/jwtGenerator.js
+++ server/utilities/jwtGenerator.js
@@ -1,11 +1,16 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+// this function generates a token for a given user against the provided
+// user_id
 function jwtGenerator(user_id) {
   const payload = {
     user: user_id,
   };
 
+  // expiresIn is for session handling -- the time duration in the string
+  // signifies the time after which the user will be signed out ie their
+  // session will expire
   return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1hr" });
 }
 
