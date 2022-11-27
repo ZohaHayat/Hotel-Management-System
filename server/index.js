cf166fd823135ce47b3c6fb6f3f8c3466c4c94f0
bakhtawar09 <24100301@lums.edu.pk>
2022-11-28 01:06:06 +0500

    comments

--- server/index.js
+++ server/index.js
@@ -1,17 +1,18 @@
 const express = require("express");
+const auth = require("./routes/jwtAuth");
 const app = express();
 const cors = require("cors");
-
+const PORT = 5000;
 //middleware
 app.use(cors());
 app.use(express.json()); // req.body
 
 //routes
-app.use("/auth", require("./routes/jwtAuth"));
+app.use("/auth", auth);
 
 // this is where you are directed to after logging in
 // app.use("/dashboard", require("./routes/dashboard"));
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log(`Server is starting on port 5000`);
 });
